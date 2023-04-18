ba44d7b9b7e6955823d5430cf2e22c879b7f85ad
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-18 18:11:40 +0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+         double temp=0;
+        if ( salary <= 10000 && salary > 0) {
+            temp = (salary*15)/100.0;
+            System.out.println(salary-temp);
+        } else if ( salary > 1000 && salary <= 20000) {
+            temp = (salary*18)/100.0;
+            System.out.println(salary-temp);
+        } else if ( salary > 20000 ) {
+            temp = (salary*20)/100.0;
+            System.out.println(salary-temp);   
+        } else if (salary < 0 ) {
+            System.out.println("wrong input!");
+        } 
     }
 }
